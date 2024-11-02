ebd4261b60d30cf3348cb635c16b123550d6f9ff
santosabby84 <159199448+abbys4@users.noreply.github.com>
2024-11-02 14:55:23 -0400

    Initial Commit

---
+++ HCI_project/HCI_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  HCI_projectApp.swift
+//  HCI_project
+//
+//  Created by Abby Santos on 11/2/24.
+//
+
+import SwiftUI
+
+@main
+struct HCI_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HCI_projectTests/HCI_projectTests.swift
@@ -0,0 +1,17 @@
+//
+//  HCI_projectTests.swift
+//  HCI_projectTests
+//
+//  Created by Abby Santos on 11/2/24.
+//
+
+import Testing
+@testable import HCI_project
+
+struct HCI_projectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
